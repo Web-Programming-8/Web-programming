7dd0002137e8d4c8ca71f2c761ac505a63ede9ec
HanTul <bluevenus02@gmail.com>
2024-12-13 20:41:34 +0900

    feat: change map and add color fill for selection and hover

--- index.js
+++ index.js
@@ -121,3 +121,70 @@ function setActiveButton(activeButton) {
   [searchNav, regionInfoNav].forEach((btn) => btn.classList.remove("active"));
   activeButton.classList.add("active");
 }
+
+
+// 시,도 지도 선택 및 색상 변경 코드
+function go_branch(city_do) {
+  var Arr = Array("sejong","chungnam","jeju","gyeongnam","gyeongbuk","jeonbuk","chungbuk","gangwon","gyeonggi","jeonnam","ulsan","busan","daegu","daejeon","incheon","seoul","gwangju");
+  var strArr = Array("세종특별자치시","충청남도","제주특별자치도","경상남도","경상북도","전라북도","충청북도","강원도","경기도","전라남도","울산광역시","부산광역시","대구광역시","대전광역시","인천광역시","서울특별시","광주광역시");
+  var idx = Arr.indexOf(city_do);
+}
+$(document).ready(function () {
+  let selectedPath = null;
+
+  // Hover 시 색상 변경
+  $(".dg-map svg").mouseover(function (event) {
+      const _path = event.target;
+      if (_path !== selectedPath) {
+          d3.select(_path).style("fill", "#cbc3ac");
+      }
+  }).mouseout(function (event) {
+      const _path = event.target;
+      if (_path !== selectedPath) {
+          d3.select(_path).style("fill", "#fff");
+      }
+  });
+
+  // 클릭 시 하나의 지역만 선택 가능
+  $(".dg-map svg").click(function (event) {
+      const _path = event.target;
+      if (selectedPath) {
+          d3.select(selectedPath).style("fill", "#fff");
+      }
+      selectedPath = _path;
+      d3.select(selectedPath).style("fill", "#cbc3ac");
+      go_branch(_path.id);
+  });
+});
+
+
+$(document).ready(function () {
+    const mapCondition = '<?=$stx?>';
+    if (mapCondition) {
+        const cityIdMap = {
+            '세종특별자치시': 'sejong',
+            '충청남도': 'chungnam',
+            '제주특별자치도': 'jeju',
+            '경상남도': 'gyeongnam',
+            '경상북도': 'gyeongbuk',
+            '전라북도': 'jeonbuk',
+            '충청북도': 'chungbuk',
+            '강원도': 'gangwon',
+            '경기도': 'gyeonggi',
+            '전라남도': 'jeonnam',
+            '울산광역시': 'ulsan',
+            '부산광역시': 'busan',
+            '대구광역시': 'daegu',
+            '대전광역시': 'daejeon',
+            '인천광역시': 'incheon',
+            '서울특별시': 'seoul',
+            '광주광역시': 'gwangju'
+        };
+
+        const cityId = cityIdMap[mapCondition];
+        if (cityId) {
+            $(`#${cityId}`).css("fill", "#cbc3ac");
+        }
+      }
+  });
+
