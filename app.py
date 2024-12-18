81e4f21efa5bc64c3a4cf46ca79c91fd86c36593
SJDKsjdk <sjdk@korea.ac.kr>
2024-12-18 11:43:21 +0900

    fix: fix the regionInfo page && port
    파주시 링크연결 수정 및 포트 4000으로 변경

--- app.py
+++ app.py
@@ -289,4 +289,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         insert_region_data()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4000, debug=True)
