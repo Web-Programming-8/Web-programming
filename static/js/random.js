752b678ed713101062431960c665045ba152f5ba
Myyoung0 <165647242+Myyoung0@users.noreply.github.com>
2024-12-18 13:59:32 +0900

    feat: random page overall change

--- static/js/random.js
+++ static/js/random.js
@@ -1,30 +1,57 @@
 const places = {
   휴식지: [
     { name: "안동 병산서원", image: "/static/images/random/andong-rd.jpg" },
-    { name: "울산 태화강 국가정원", image: "/static/images/random/ulsan-rd.jpg" },
+    {
+      name: "울산 태화강 국가정원",
+      image: "/static/images/random/ulsan-rd.jpg",
+    },
     { name: "태안 네이처월드", image: "/static/images/random/taean-rd.jpg" },
-    { name: "춘천 제이드 가든", image: "/static/images/random/chuncheon-rd.jpg" },
+    {
+      name: "춘천 제이드 가든",
+      image: "/static/images/random/chuncheon-rd.jpg",
+    },
     { name: "제주 비자림", image: "/static/images/random/jeju-rd.jpg" },
     { name: "군산 선유도", image: "/static/images/random/gunsan-rd.jpg" },
     { name: "제천 의림지", image: "/static/images/random/jecheon-rd.jpg" },
     { name: "남해 섬이정원", image: "/static/images/random/namhae-rd.jpg" },
     { name: "포천 산정호수", image: "/static/images/random/pocheon-rd.jpg" },
-    { name: "횡성 미술관 자작나무숲", image: "/static/images/random/hoengseong-rd.jpg" },
+    {
+      name: "횡성 미술관 자작나무숲",
+      image: "/static/images/random/hoengseong-rd.jpg",
+    },
   ],
   액티비티: [
     { name: "서울 스카이", image: "/static/images/random/seoul-rd.jpg" },
     { name: "제주 9.81 파크", image: "/static/images/random/jeju981-rd.jpg" },
     { name: "영월 어라연", image: "/static/images/random/yeong-wol-rd.jpg" },
-    { name: "남양주 웨이브서프", image: "/static/images/random/namyangju-rd.jpg" },
-    { name: "단양 만천하 스카이워크", image: "/static/images/random/danyang-rd.jpg" },
-    { name: "영인 플라이스테이션코리아", image: "/static/images/random/yeongin-rd.jpg" },
+    {
+      name: "남양주 웨이브서프",
+      image: "/static/images/random/namyangju-rd.jpg",
+    },
+    {
+      name: "단양 만천하 스카이워크",
+      image: "/static/images/random/danyang-rd.jpg",
+    },
+    {
+      name: "영인 플라이스테이션코리아",
+      image: "/static/images/random/yeongin-rd.jpg",
+    },
     { name: "통영 케이블카", image: "/static/images/random/tongyeong-rd.jpg" },
-    { name: "화성 전곡항 마리나", image: "/static/images/random/hwaseong-rd.jpg" },
-    { name: "하동 알프스 레포츠", image: "/static/images/random/hadong-rd.jpg" },
+    {
+      name: "화성 전곡항 마리나",
+      image: "/static/images/random/hwaseong-rd.jpg",
+    },
+    {
+      name: "하동 알프스 레포츠",
+      image: "/static/images/random/hadong-rd.jpg",
+    },
     { name: "제주 빅볼랜드", image: "/static/images/random/jejubig-rd.jpg" },
   ],
   감성: [
-    { name: "경주 엑스포대공원", image: "/static/images/random/gyeongju-rd.jpg" },
+    {
+      name: "경주 엑스포대공원",
+      image: "/static/images/random/gyeongju-rd.jpg",
+    },
     { name: "제천 금봉월", image: "/static/images/random/jecheongum-rd.jpg" },
     { name: "부산 우암동 도시숲", image: "/static/images/random/busan-rd.jpg" },
     { name: "수원 월화원", image: "/static/images/random/suwon-rd.jpg" },
@@ -32,19 +59,43 @@ const places = {
     { name: "삼척 미인폭포", image: "/static/images/random/samcheok-rd.jpg" },
     { name: "완주 아원고택", image: "/static/images/random/wanju-rd.jpg" },
     { name: "강릉 안반데기", image: "/static/images/random/gangneung-rd.jpg" },
-    { name: "제주 도두동 무지개해안도로", image: "/static/images/random/jejudodu-rd.jpg" },
+    {
+      name: "제주 도두동 무지개해안도로",
+      image: "/static/images/random/jejudodu-rd.jpg",
+    },
     { name: "서울 이랜드크루즈", image: "/static/images/random/seoule-rd.jpg" },
   ],
   신상여행지: [
-    { name: "창원 마산로봇랜드", image: "/static/images/random/changwon-rd.jpg" },
-    { name: "서울 젠틀몬스터 하우스도산", image: "/static/images/random/seoulj-rd.jpg" },
-    { name: "통영 욕지섬모노레일", image: "/static/images/random/tongyeongsum-rd.jpg" },
-    { name: "밀양 아리랑 우주천문대", image: "/static/images/random/miryang-rd.jpg" },
-    { name: "남해 설리스카이워크", image: "/static/images/random/namhaes-rd.jpg" },
-    { name: "대전 소제동 관사촌", image: "/static/images/random/daejeon-rd.jpg" },
+    {
+      name: "창원 마산로봇랜드",
+      image: "/static/images/random/changwon-rd.jpg",
+    },
+    {
+      name: "서울 젠틀몬스터 하우스도산",
+      image: "/static/images/random/seoulj-rd.jpg",
+    },
+    {
+      name: "통영 욕지섬모노레일",
+      image: "/static/images/random/tongyeongsum-rd.jpg",
+    },
+    {
+      name: "밀양 아리랑 우주천문대",
+      image: "/static/images/random/miryang-rd.jpg",
+    },
+    {
+      name: "남해 설리스카이워크",
+      image: "/static/images/random/namhaes-rd.jpg",
+    },
+    {
+      name: "대전 소제동 관사촌",
+      image: "/static/images/random/daejeon-rd.jpg",
+    },
     { name: "경남 가야랜드", image: "/static/images/random/gyeongnam-rd.jpg" },
     { name: "강원 뮤지엄산", image: "/static/images/random/gangwon-rd.jpg" },
-    { name: "서울 성수 공간와디즈", image: "/static/images/random/seoulg-rd.jpg" },
+    {
+      name: "서울 성수 공간와디즈",
+      image: "/static/images/random/seoulg-rd.jpg",
+    },
     { name: "속초 갯배st", image: "/static/images/random/sokcho-rd.jpg" },
   ],
 };
@@ -64,26 +115,26 @@ function playBgm() {
   bgm.play().catch((err) => console.error("배경음악 재생 오류:", err));
 }
 
-function hideLoading() {
+function showLoading() {
   const loadingElement = document.getElementById("loading");
   if (loadingElement) {
-    loadingElement.style.display = "none";
+    loadingElement.classList.remove("hidden"); // hidden 클래스 제거
   } else {
     console.error("로딩 요소를 찾을 수 없습니다.");
   }
 }
 
-function showLoading() {
+function hideLoading() {
   const loadingElement = document.getElementById("loading");
   if (loadingElement) {
-    loadingElement.style.display = "flex";
+    loadingElement.classList.add("hidden"); // hidden 클래스 추가
   } else {
     console.error("로딩 요소를 찾을 수 없습니다.");
   }
 }
 
 window.onload = function () {
-  hideLoading();
+  hideLoading(); // 초기 로딩 창 숨기기
 };
 
 function startRandomRecommendation() {
